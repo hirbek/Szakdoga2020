95b2e3d1e2411813a33836d3e62eb97421f44690
Hirbek András <hirbek.andras@gmail.com>
2020-02-07 00:44:21 +0100

    Calendar +events

--- Szakdoga/Controllers/HomeController.cs
+++ Szakdoga/Controllers/HomeController.cs
@@ -3,28 +3,97 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Szakdoga.Models;
 
-namespace Szakdoga.Controllers
+namespace Szakdolgozat.Controllers
 {
     public class HomeController : Controller
     {
+        readonly ApplicationDbContext _context;
+
+        public HomeController() => _context = new ApplicationDbContext();
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Calendar()
         {
-            ViewBag.Message = "Your application description page.";
+
 
             return View();
         }
-
-        public ActionResult Contact()
+        public ActionResult Event()
         {
-            ViewBag.Message = "Your contact page.";
 
-            return View();
+            var events = _context.FoNaptar.ToList();
+            return View("Event", events);
         }
+        public JsonResult GetEvents()
+        {
+
+            var events = _context.FoNaptar.ToList();
+            return new JsonResult { Data = events, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+        [HttpPost]
+        public JsonResult SaveEvent(MainCalendar e)
+        {
+            var status = false;
+            {
+                if (e.EventId > 0)
+                {
+                    //Update the event
+                    var v = _context.FoNaptar.Where(a => a.EventId == e.EventId).FirstOrDefault();
+                    if (v != null)
+                    {
+                        v.Subject = e.Subject;
+                        v.Start = e.Start;
+                        v.End = e.End;
+                        v.Description = e.Description;
+
+                    }
+                }
+                else
+                {
+                    _context.FoNaptar.Add(e);
+                }
+
+                _context.SaveChanges();
+                status = true;
+
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+
+        public ActionResult Edit(MainCalendar Event)
+        {
+            if (Event.EventId == 0)
+            {
+                _context.FoNaptar.Add(Event);
+            }
+            else
+            {
+                var letezoEvent = _context.FoNaptar.Single(m => m.EventId == Event.EventId);
+                letezoEvent.Subject = Event.Subject;
+                letezoEvent.Description = Event.Description;
+                letezoEvent.Start = Event.Start;
+                letezoEvent.End = Event.End;
+            }
+            _context.SaveChanges();
+            return RedirectToAction("Calendar");
+        }
+        public ActionResult Delete(int Id)
+        {
+            if (Id == 0)
+            {
+                return HttpNotFound();
+            }
+            var letezoEvent = _context.FoNaptar.Find(Id);
+            _context.FoNaptar.Remove(letezoEvent);
+            _context.SaveChanges();
+            return RedirectToAction("Event");
+        }
+
+
     }
 }
